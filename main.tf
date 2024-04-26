5dda01504a49f6dcbc989bec506704b095dac3d7
anushkaw98 <137258770+anushkaw98@users.noreply.github.com>
2024-04-26 09:43:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,22 +17,3 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.prod-vpc.id
 
 }
-
-#create custom route table
-resource "aws_route_table" "prod-route-table" {
-  vpc_id = aws_vpc.prod-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id             = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "prod"
-  }
-}
